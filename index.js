5ab57fc8ff30b3fc548c2194a0c057afdb1b8721
Mo Yang <moyang313@gmail.com>
2022-04-17 19:21:16 -0400

    add questions for different team member roles, work in progress

--- index.js
+++ index.js
@@ -5,88 +5,305 @@ const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
 const generatePage = require('./src/page-template');
 
-
-// TODO: Create an array of questions for user input
-const questions = [
+const managerQuestions = () => {
+    return inquirer.prompt([
         {
-            type: "input",
-            name: "title",
-            message: "What is the title of your project?",
-        },
-        {
-            type: "input",
-            name: "description",
-            message: "Please enter a description of your project:"
-        },
-        {
-            type: "input",
-            name: "installation",
-            message: "Please provide installation instructions:",
-        },
-        {
-            type: "input",
-            name: "usage",
-            message: "Please provide usage information:",
-        },
-        {
-            type: "input",
-            name: "contribution",
-            message: "What are the contribution guidelines?"
-        },
-        {
-            type: "input",
-            name: "test",
-            message: "What are the test instructions?"
-        },
-        {
-            type: "list",
-            name: "license",
-            message: "Please select a license for your application from the list of options:",
-            choices: ['MIT', 'GNU', 'Apache', 'BSD', 'ISC', 'Other']
-        },
-        {
-            type: "input",
-            name: "github",
-            message: "What is your GitHub Username?"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is your email address?"
+        type: "input",
+        name: "name",
+        message: "What is the team manager's name? (Required)",
+        validate: nameInput => {
+            if (nameInput) {
+            return true;
+            } else {
+            console.log('Please enter a team manager name!');
+            return false;
+            }
         }
-    ]
+        },
+        {
+        type: "input",
+        name: "id",
+        message: "Please enter an employee ID (Required)",
+        validate: idInput => {
+            if (idInput) {
+            return true;
+            } else {
+            console.log('Please enter an employee ID!');
+            return false;
+            }
+        }
+        },
+        {
+        type: "input",
+        name: "email",
+        message: "Please enter an email address (Required)",
+        validate: emailInput => {
+            if (emailInput) {
+            return true;
+            } else {
+            console.log('Please enter an email address!');
+            return false;
+            }
+        }
+        },
+        {
+        type: "input",
+        name: "phone",
+        message: "Please enter an office number (Required)",
+        validate: phoneInput => {
+            if (phoneInput) {
+            return true;
+            } else {
+            console.log('Please enter an office number!');
+            return false;
+            }
+        }
+        },
+        {
+        type: "list",
+        name: "addTeamMember",
+        message: "Please choose if you would like to add additional team members:",
+        choices: ['Add Engineer', 'Add Intern', 'No more members'],
+        validate: addTeamMemberInput => {
+            if (addTeamMemberInput) {
+                return true;
+            } else {
+                console.log('You need to make a selection!');
+                return false;
+            }
+        }
+        }
+    ]);
+};
 
-// TODO: Create a function to initialize app and write README file
-const init = () => {
-    return inquirer.prompt(questions)
-    .then(answers => {
-        const data = generatePage(answers);
-        console.log(answers);
-        fs.writeFile('./dist/index.html', data, err => {
-        if(err) throw new Error(err);
-            console.log('Team Profile page is created!');
-        });
-    });
+const engineerQuestions = () => {
+    return inquirer.prompt([
+        {
+        type: "input",
+        name: "name",
+        message: "What is the engineer's name? (Required)",
+        validate: nameInput => {
+            if (nameInput) {
+            return true;
+            } else {
+            console.log('Please enter a name!');
+            return false;
+            }
+        }
+        },
+        {
+        type: "input",
+        name: "id",
+        message: "Please enter an employee ID (Required)",
+        validate: idInput => {
+            if (idInput) {
+            return true;
+            } else {
+            console.log('Please enter an employee ID!');
+            return false;
+            }
+        }
+        },
+        {
+        type: "input",
+        name: "email",
+        message: "Please enter an email address (Required)",
+        validate: emailInput => {
+            if (emailInput) {
+            return true;
+            } else {
+            console.log('Please enter an email address!');
+            return false;
+            }
+        }
+        },
+        {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter a GitHub Username (Required)',
+        validate: githubInput => {
+            if (githubInput) {
+            return true;
+            } else {
+            console.log('Please enter a GitHub username!');
+            return false;
+            }
+        }
+        },
+        {
+        type: "list",
+        name: "addTeamMember",
+        message: "Please choose if you would like to add additional team members:",
+        choices: ['Add Engineer', 'Add Intern', 'No more members'],
+        validate: addTeamMemberInput => {
+            if (addTeamMemberInput) {
+                return true;
+            } else {
+                console.log('You need to make a selection!');
+                return false;
+            }
+        }
+        }
+    ]);
+};
+
+const internQuestions = () => {
+    return inquirer.prompt([
+        {
+        type: "input",
+        name: "name",
+        message: "What is the engineer's name? (Required)",
+        validate: nameInput => {
+            if (nameInput) {
+            return true;
+            } else {
+            console.log('Please enter a name!');
+            return false;
+            }
+        }
+        },
+        {
+        type: "input",
+        name: "id",
+        message: "Please enter an employee ID (Required)",
+        validate: idInput => {
+            if (idInput) {
+            return true;
+            } else {
+            console.log('Please enter an employee ID!');
+            return false;
+            }
+        }
+        },
+        {
+        type: "input",
+        name: "email",
+        message: "Please enter an email address (Required)",
+        validate: emailInput => {
+            if (emailInput) {
+            return true;
+            } else {
+            console.log('Please enter an email address!');
+            return false;
+            }
+        }
+        },
+        {
+        type: 'input',
+        name: 'school',
+        message: 'Please enter a school name (Required)',
+        validate: schoolInput => {
+            if (schoolInput) {
+            return true;
+            } else {
+            console.log('Please enter school name!');
+            return false;
+            }
+        }
+        },
+        {
+        type: "list",
+        name: "addTeamMember",
+        message: "Please choose if you would like to add additional team members:",
+        choices: ['Add Engineer', 'Add Intern', 'No more members'],
+        validate: addTeamMemberInput => {
+            if (addTeamMemberInput) {
+                return true;
+            } else {
+                console.log('You need to make a selection!');
+                return false;
+            }
+        }
+        }
+    ]);
+};
+
+const promptAddMemeber = teamData => {
+console.log(`
+=================
+Add a New Team Memeber
+=================
+`);
+
+
+// If there's no 'projects' array property, create one
+if (!teamData.projects) {
+    teamData.projects = [];
 }
-
-// copying file
-const copyFile = () => {
-    return new Promise((resolve, reject) => {
-      fs.copyFile('./src/style.css', './dist/style.css', err => {
-        if (err) {
-          reject(err);
-          return;
+return inquirer
+    .prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your project? (Required)',
+        validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('You need to enter a project name!');
+            return false;
         }
-  
-        resolve({
-          ok: true,
-          message: 'Stylesheet created!'
-        });
-      });
+        }
+    },
+    {
+        type: "list",
+        name: "addTeamMember",
+        message: "Please choose if you would like to add additional team members:",
+        choices: ['Add Engineer', 'Add Intern', 'No more members'],
+        validate: addTeamMemberInput => {
+            if (addTeamMemberInput) {
+                return true;
+            } else {
+                console.log('You need to make a selection!');
+                return false;
+            }
+            }
+    },
+    {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: linkInput => {
+        if (linkInput) {
+            return true;
+        } else {
+            console.log('You need to enter a project GitHub link!');
+            return false;
+        }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like to feature this project?',
+        default: false
+    },
+    {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'Would you like to enter another project?',
+        default: false
+    }
+    ])
+    .then(memberData => {
+    teamData.projects.push(memberData);
+    if (memberData.confirmAddProject) {
+        return promptAddMemeber(teamData);
+    } else {
+        return teamData;
+    }
     });
-  };
+};
 
-// Function call to initialize app
-init();
-copyFile();
+managerQuestions()
+.then(promptAddMemeber)
+.then(teamData => {
+    const pageHTML = generatePage(teamData);
+
+    fs.writeFile('./dist/index.html', pageHTML, err => {
+    if (err) throw new Error(err);
+
+    console.log('Page created! Check out index.html in this directory to see it!');
+    });
+});
 
